e3bcfbbb0affcf26f495bc3d1bad478f74abed21
Dylan King <kingda16@wfu.edu>
2017-11-03 16:56:22 -0400

    2 dimensional gradient flow is up and running

--- code/2d/grad2d.m
+++ code/2d/grad2d.m
@@ -1,10 +1,25 @@
-function [udot] = grad1d(t,u,DX,DXX,DXXXX,delta,epsilon)
-%u = sparse(u);
+function [udot] = grad2d(t,u,DX,DXX,DXXXX,delta,epsilon,N)
+
+
+u=reshape(u,[N,N]);
+
 ux = DX*u;
 uxx = DXX*u;
 uxxxx = DXXXX*u;
-udot=-(2*delta*u-4*(3*uxx.*(ux.^2)-uxx)+2*epsilon^2*uxxxx);
-udot(1)=0;
-udot(end)=0;
+uy = (DX*u')';
+uyy = (DXX*u')';
+uyyyy = (DXXXX*u')';
+uxy = DX*uy;
+uxxyy = DXX*uyy;
+
+udot = -(2*delta*u-4*(ux.*(2*uxx+2*uxy)+uy.*(2*uyy+2*uxy)+(uxx+uyy).*(ux.^2+uy.^2-1))+2*epsilon.^2*(uxxxx+uyyyy+2*uxxyy));
+
+udot(1,:) = 0;
+udot(end,:) = 0;
+udot(:,1) = 0;
+udot(:,end) = 0;
+
+
+udot=reshape(udot,[N*N,1]);
 end
 
