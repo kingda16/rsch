6959976f521ad84b927e22a7773ef5127f958d02
Dylan King <kingda16@wfu.edu>
2017-11-10 15:49:54 -0500

    syncing

--- code/2d/data/vis.m
+++ code/2d/data/vis.m
@@ -1,3 +1,51 @@
+
+
+dx = x(1,2) - x(1,1);
+DX=(-1*diag(ones(1,n-1),-1)+1*diag(ones(1,n-1),1));
+DX(1,1) = -2;
+DX(1,2) = 2;
+DX(end,end) = 2;
+DX(end,end-1) = -2;
+
+
+DXX=(1*diag(ones(1,n-1),-1)+1*diag(ones(1,n-1),1)-2*diag(ones(1,n)));
+DXX(1,1) = 0;
+DXX(1,2) = 0;
+DXX(end,end) = 0;
+DXX(end,end-1) = 0;
+
+DXXXX=(1*diag(ones(1,n-2),-2)+1*diag(ones(1,n-2),2)-4*diag(ones(1,n-1),-1)-4*diag(ones(1,n-1),1)+6*diag(ones(1,n)));
+DXXXX(1,1) = -5;
+DXXXX(1,2) = 14;
+DXXXX(1,3) = -14;
+DXXXX(1,4) = 6;
+DXXXX(1,5) = -1;
+
+DXXXX(end,end) = -5;
+DXXXX(end,end-1) = 14;
+DXXXX(end,end-2) = -14;
+DXXXX(end,end-3) = 6;
+DXXXX(end,end-4) = -1;
+
+DXXXX(2,1) = -2;
+DXXXX(2,2) = 5;
+DXXXX(2,3) = -4;
+DXXXX(2,4) = 1;
+
+DXXXX(end-1,end) = -2;
+DXXXX(end-1,end-1) = 5;
+DXXXX(end-1,end-2) = -4;
+DXXXX(end-1,end-3) = 1;
+
+
+DX=1/(2*dx)*DX;
+DXX = 1/(dx^2)*DXX;
+DXXXX=1/(dx^4)*DXXXX;
+
+DXXXX = sparse(DXXXX);
+DXX = sparse(DXX);
+DX = sparse(DX);
+
 vidObj = VideoWriter('test.avi');
 open(vidObj);
 
@@ -7,9 +55,10 @@ for k=1:M,
         clf;
     out = reshape(u(k,:),[n,n]);
     %contourf(out,200,'Linestyle','none')
-
-    surf(x,y,out);
-    view(-37.5,30);
+    contourf(1-sqrt((DX*out).^2+((DX*(out')).^2)'),linspace(-0.2,0.2,100),'LineStyle','none');
+    %surf(x,y,out);
+    %view(-37.5,30);
+    colorbar
 shg;
 zlim([-1 1])
     currFrame = getframe(gcf);
--- utility/fetchpsftp.bat
+++ utility/fetchpsftp.bat
@@ -1,4 +1,4 @@
 cd /deac/generalGrp/gemmerGrp/DAK/code/2d/data
 lcd ..
 lcd code/2d/data
-mget *
+mget e*
