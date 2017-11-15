fad747b7c7b90a9af3a62db9ba7665f94efe4e7b
Dylan King <kingda16@wfu.edu>
2017-11-15 15:14:46 -0500

    Multipoint

--- code/2d/gflow2d.m
+++ code/2d/gflow2d.m
@@ -66,21 +66,30 @@ DXX = sparse(DXX);
 DX = sparse(DX);
 
 %% Construction of initial profile
-% for i = 1:n
-%     for j = 1:n
-%         guess(i,j) = max([0.2*power(10*x(1,i).*(1-x(1,i)).*(1-y(j,1)).*y(j,1),1/4),min([min([1-x(1,i),1-y(j,1),x(1,i)]),1-max([1-x(1,i),1+offset-y(j,1),x(1,i)])])]);
-%     end
-% end
+%for i = 1:n
+%    for j = 1:n
+%        guess(i,j) = max([0.2*power(10*x(1,i).*(1-x(1,i)).*(1-y(j,1)).*y(j,1),1/4),min([min([1-x(1,i),1-y(j,1),x(1,i)]),1-max([1-x(1,i),1+offset-y(j,1),x(1,i)])])]);
+%    end
+%end
+
+
+%for i = 1:n
+%for j = 1:n
+%guess(i,j) = max([0.2*power(10*x(1,i).*(1-x(1,i)).*(1-y(j,1)).*y(j,1),1/4),min([1-x(1,i),x(1,i),y(j,1),1-y(j,1)])]);
+%end
+%end
 
 
 for i = 1:n
-for j = 1:n
-guess(i,j) = max([0.2*power(10*x(1,i).*(1-x(1,i)).*(1-y(j,1)).*y(j,1),1/4),min([1-x(1,i),x(1,i),y(j,1),1-y(j,1)])]);
+    for j = 1:floor(n/2)
+        guess(i,j) = min([x(1,j),0.5-x(1,j),y(i,1),1-y(i,1)]);
+    end
 end
+for i = 1:n
+    for j = floor(n/2)+1:n
+        guess(i,j) = min(abs([y(i,1)-0.5,0.5-y(i,1),x(1,j)-0.5,1-x(1,j),2*y(i,1),2-2*y(i,1)]));
+    end
 end
-
-
-
 
 
 
