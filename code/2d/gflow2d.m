bf5f5c78e8e0bd5099be7d34b206358110962cf6
Dylan King <kingda16@wfu.edu>
2017-12-10 18:04:54 -0500

    seedgen worked in

--- code/2d/gflow2d.m
+++ code/2d/gflow2d.m
@@ -1,4 +1,4 @@
-function [] = gflow2d( delta,epsilon,n,M,T,offset)
+function [] = gflow2d( delta,epsilon,n,M,T,seed)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   Dylan King
@@ -65,53 +65,7 @@ DXXXX = sparse(DXXXX);
 DXX = sparse(DXX);
 DX = sparse(DX);
 
-
-%% Construction of initial profile
-%for i = 1:n
-%    for j = 1:n
-%        guess(i,j) = max([0.2*power(10*x(1,i).*(1-x(1,i)).*(1-y(j,1)).*y(j,1),1/4),min([min([1-x(1,i),1-y(j,1),x(1,i)]),1-max([1-x(1,i),1+offset-y(j,1),x(1,i)])])]);
-%    end
-%end
-
-
-%for i = 1:n
-%for j = 1:n
-%guess(i,j) = max([0.2*power(10*x(1,i).*(1-x(1,i)).*(1-y(j,1)).*y(j,1),1/4),min([1-x(1,i),x(1,i),y(j,1),1-y(j,1)])]);
-%end
-%end
-
-
-%for i = 1:n
-%    for j = 1:floor(n/2)
-%        guess(i,j) = min([x(1,j),0.5-x(1,j),y(i,1),1-y(i,1)]);
-%    end
-%end
-%for i = 1:n
-%    for j = floor(n/2)+1:n
-%        guess(i,j) = min(abs([y(i,1)-0.5,0.5-y(i,1),x(1,j)-0.5,1-x(1,j),2*y(i,1),2-2*y(i,1)]));
-%    end
-%end
-
-
-
-
-% for i =1:n
-%     for j = 1:n
-%         guess(i,j) = max([0.4*power(10*x(1,j).*(1-x(1,j)).*(1-y(i,1)).*y(i,1),1/4),guess(i,j)]);
-%     end
-% end
-
-guess = sin(pi*x).*sin(pi*y);
-
-
-%guess = 0.25*sin(7*pi*x).*sin(7*pi*y);
-
-guess(1,:) = 0; 
-guess(end,:) = 0;
-guess(:,1) = 0;
-guess(:,end) = 0;
-
-guess=reshape(guess,[n*n,1]);
+guess=reshape(seed,[n*n,1]);
 grad2d(0,guess,DX,DXX,DXXXX,delta,epsilon,n);
 
 options = odeset('Stats','on');
