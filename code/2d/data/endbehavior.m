1bdadc36f421b05a285b6c62fbcf3c1c4d3dca6b
Dylan King <kingda16@wfu.edu>
2018-03-11 23:35:19 -0400

    syncing

--- code/2d/data/endbehavior.m
+++ code/2d/data/endbehavior.m
@@ -50,14 +50,14 @@ DX = sparse(DX);
 epsx = zeros(19,1);
 energy = zeros(19,1);
 
-xt = zeros(31,31);
-yt = zeros(31,31);
-zt = zeros(31,31);
+xt = zeros(11,11);
+yt = zeros(11,11);
+zt = zeros(11,11);
 
-for i = 0:30
-    for j = 0:30
-        eps = 0.02+i/30*(0.2-0.02);
-        del = j/30*(10);
+for i = 0:10
+    for j = 0:10
+        eps = 0.02+i/10*(0.2-0.02);
+        del = j/10*(10);
         xt(i+1,j+1) = eps;
         yt(i+1,j+1) = del;
         try
@@ -70,6 +70,75 @@ for i = 0:30
         end
     end
 end
+
+
+n = 100;
+dx = 1/(n+1);
+DX=(-1*diag(ones(1,n-1),-1)+1*diag(ones(1,n-1),1));
+DX(1,1) = -2;
+DX(1,2) = 2;
+DX(end,end) = 2;
+DX(end,end-1) = -2;
+
+
+DXX=(1*diag(ones(1,n-1),-1)+1*diag(ones(1,n-1),1)-2*diag(ones(1,n)));
+DXX(1,1) = 0;
+DXX(1,2) = 0;
+DXX(end,end) = 0;
+DXX(end,end-1) = 0;
+
+DXXXX=(1*diag(ones(1,n-2),-2)+1*diag(ones(1,n-2),2)-4*diag(ones(1,n-1),-1)-4*diag(ones(1,n-1),1)+6*diag(ones(1,n)));
+DXXXX(1,1) = -5;
+DXXXX(1,2) = 14;
+DXXXX(1,3) = -14;
+DXXXX(1,4) = 6;
+DXXXX(1,5) = -1;
+
+DXXXX(end,end) = -5;
+DXXXX(end,end-1) = 14;
+DXXXX(end,end-2) = -14;
+DXXXX(end,end-3) = 6;
+DXXXX(end,end-4) = -1;
+
+DXXXX(2,1) = -2;
+DXXXX(2,2) = 5;
+DXXXX(2,3) = -4;
+DXXXX(2,4) = 1;
+
+DXXXX(end-1,end) = -2;
+DXXXX(end-1,end-1) = 5;
+DXXXX(end-1,end-2) = -4;
+DXXXX(end-1,end-3) = 1;
+
+
+DX=1/(2*dx)*DX;
+DXX = 1/(dx^2)*DXX;
+DXXXX=1/(dx^4)*DXXXX;
+
+DXXXX = sparse(DXXXX);
+DXX = sparse(DXX);
+DX = sparse(DX);
+
+
+
+
+
+for i = 0:10
+    for j = 0:10
+        eps = 0.02+i/10*(0.2-0.02);
+        del = j/10*(10);
+        try
+            
+            load(strcat('e',num2str(eps),'d',num2str(del),'n100m50t0.03.mat'));
+            zt(i+1,j+1) = zt(i+1,j+1)- functional2d(reshape(u(end,:),[n,n]),DX,DXX,del,eps);
+            
+        catch ME
+        
+        end
+    end
+end
+
+
 surf(xt,yt,zt)
 xlabel('\epsilon');
 ylabel('\delta');
